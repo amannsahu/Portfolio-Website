18cf2e49f320f266a5a08bcd37076fc9b70f4951
amannsahu <sahuarya004@gmail.com>
2024-10-24 17:18:07 +0530

    change1

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
   return (
     <div id='about' className=' mt-16'>
       <h1 className=' font-semibold text-7xl text-white-100'>About Me</h1>
-      <div className='flex flex-col lg:gap-12 lg:flex lg:flex-row lg:items-center mt-12'>
-        <div className='ml-28'>
+      <div className='flex flex-col items-center lg:gap-12 lg:flex lg:flex-row lg:items-center pt-16'>
+        <div className=''>
             <img src={image} width={375} className=' rounded-full'/>
         </div>
 
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <div className='flex flex-col gap-4'>
-        <div className='h-[1px] w-11/12 mx-auto bg-white-100'></div>
+        <div className='h-[1px] lg:w-11/12 max-md:w-[200px] max-lg:w-[750px] mx-auto bg-white-100'></div>
         <div className='text-white-100 mb-8'>
             &#169; 2024 All rights reserved
         </div>
